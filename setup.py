e40f6e76064c346bccc308a431650fe87e21e40a
Azat Ibrakov <azatibrakov@gmail.com>
2025-11-20 03:34:03 +0100

    Complete package metadata

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 import sys
 
-from setuptools import (find_packages,
-                        setup)
+from setuptools import find_packages, setup
 
 project_base_url = 'https://github.com/{{github_login}}/{{project}}/'
-parameters = dict(packages=find_packages(exclude=('tests', 'tests.*')),
-                  url=project_base_url,
-                  download_url=project_base_url + 'archive/master.zip')
+parameters = dict(
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    url=project_base_url,
+    download_url=project_base_url + 'archive/master.zip',
+)
 if sys.implementation.name == 'cpython':
     from setuptools_rust import RustExtension
 
     parameters.update(
-            rust_extensions=[RustExtension('{{project}}._{{project}}')],
-            zip_safe=False
+        rust_extensions=[RustExtension('{{project}}._{{project}}')],
+        zip_safe=False
     )
 setup(**parameters)
